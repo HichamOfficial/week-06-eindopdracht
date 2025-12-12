d9752e5d7ffd88303cac7de89c82ddf75bb0fce1
HichamOfficial <s1183761@student.windesheim.nl>
2025-12-13 00:58:11 +0100

    Fix FMT

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,28 +1,28 @@
 variable "esxi_mngmt_host" {
-  type        = string
-  default     = "192.168.1.60"
+  type    = string
+  default = "192.168.1.60"
 }
 
 variable "esxi_mngmt_ssh_port" {
-  type        = number
-  default     = 22
+  type    = number
+  default = 22
 }
 
 variable "esxi_mngmt_https_port" {
-  type        = number
-  default     = 443
+  type    = number
+  default = 443
 }
 
 variable "esxi_mngmt_user" {
-  type        = string
+  type = string
 }
 
 variable "esxi_mngmt_password" {
-  type        = string
-  sensitive   = true
+  type      = string
+  sensitive = true
 }
 
 variable "azure_mngmt_subscription_id" {
-  type        = string
-  sensitive   = true
+  type      = string
+  sensitive = true
 }
